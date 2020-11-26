31c56c29c23b28dcbd288f45fd0cb085c82c6b74
Mészáros Mátyás <34501282+mmatyi@users.noreply.github.com>
2020-11-26 21:03:58 +0100

    Update install.R

--- .binder/install.R
+++ .binder/install.R
@@ -1,3 +1,3 @@
-install.packages(c("reshape2","cranlogs","DT"))
+install.packages(c("reshape2","cranlogs","DT","cron"))
 devtools::install_github('eurostat/restatapi')
 devtools::install_github('ramnathv/rCharts')
