f1f1a461a5d7a00495814ec49e83613f020a0e15
Mészáros Mátyás <34501282+mmatyi@users.noreply.github.com>
2020-12-13 00:18:20 +0100

    Update get_log.R

--- cran_logs/get_log.R
+++ cran_logs/get_log.R
@@ -1,8 +1,7 @@
 if (!("data.table" %in% rownames(installed.packages()))){install.packages(data.table)}
 library(data.table)
-cat(getwd())
-old_rlogs<-readRDS(".cran_logs/rlogs.RDS")
+old_rlogs<-readRDS("./cran_logs/rlogs.RDS")
 rlogs_new<-data.table(date=Sys.Date(),time=format(Sys.time()),v=round(runif(1,0,100)))
 rlogs<-rbind(old_rlogs,rlogs_new)
-saveRDS(rlogs,"rlogs.RDS")
-fwrite(rlogs,"rlogs.tsv",sep="\t")
+saveRDS(rlogs,"./cran_logs/rlogs.RDS")
+fwrite(rlogs,"./cran_logs/rlogs.tsv",sep="\t")
