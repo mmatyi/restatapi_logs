c359e5109e074be450bae7d1e7ccaec2f19c2da1
Mészáros Mátyás Tamás <matyas.meszaros@ec.europa.eu>
2020-12-13 00:29:38 +0000

    detailed stat shiny app

---
+++ ShinyApps/restatapi/server.R
@@ -0,0 +1,111 @@
+library(shiny)
+library(rCharts)
+# library(rMaps)
+# library(reshape2)
+# library(cranlogs)
+library(data.table)
+
+
+shinyServer(function(input, output) {
+  
+  adat<-reactive({
+    input$update
+    isolate({
+      withProgress({
+        setProgress(message = "Filtering data...")
+        d<-readRDS("../../cran_logs//rlogs.RDS")
+        d<-d[date>=input$sdatum & date<=input$edatum]
+        return(d)
+      })
+    })
+    
+ })   
+  output$sumText  <- renderText({
+     if (nrow(adat())==0) {
+      paste0("No downloads between ",as.character(input$sdatum)," and ",as.character(input$edatum),".")
+   } else {
+      paste("The total download in the period:", as.character(nrow(adat())))
+    }
+  })
+
+ 
+  
+  
+  output$adattabla <- DT::renderDataTable(DT::datatable({
+    adat()[order(-date)]
+  },rownames=FALSE,options = list(paging = FALSE,column.searchable = T,info=FALSE)))
+
+
+
+  
+  dd1<-reactive({
+  dd1<-adat()[,c("version","r_os")]
+  dd1[grepl("linux",r_os),os:="linux"]
+  dd1[grepl("darwin",r_os),os:="mac"]
+  dd1[grepl("mingw",r_os),os:="windows"]
+  dd1<-dd1[,.(dd=.N),by=c("version","os")]
+  dd1<-dd1[!is.na(os)]
+  dd1
+  })
+  
+  
+  output$versiongraph <- renderChart({
+    n1 <- nPlot(dd ~ version, group = "os", data = dd1(), type = "multiBarChart")
+    n1$addParams(dom = 'versiongraph')
+    n1$addParams(reduceXTicks=FALSE)
+    return(n1)
+
+  })
+
+  
+  
+  dd<-reactive({
+    dd<-adat()[,c("date")]
+    dd<-dd[,.(dd=.N),by=date]
+    dd$cumsum<-cumsum(dd$dd)
+    dd$date<-as.Date(dd$date)
+    
+    dd$y<- format(dd$date, "%Y")
+    dd$m<- as.numeric(format(dd$date, "%m"))-1
+    dd$d<- format(dd$date, "%d")
+    dd$date2<-paste0("Date.UTC(",dd$y,",",dd$m,",",dd$d,")")
+    dd
+  })
+  
+  
+  
+  output$myChart3 <- renderChart({
+    h <- Highcharts$new()
+    h$addParams(dom = 'myChart3')
+    return(h)
+    
+  })
+  
+    output$plot2 <- renderText({  
+    dd()[order(date)]
+    
+    s1.json <- toJSONArray2(cbind(dd()$date2, dd()$dd), json = T, names = F)
+    s2.json <- toJSONArray2(cbind(dd()$date2, dd()$cumsum), json = T, names = F)
+  
+      h1 <- Highcharts$new()
+    h1$chart(type = "scatter")
+    h1$chart(zoomType = "xy")
+    h1$xAxis(type = "datetime", labels=list(format= "{value:%Y-%m-%d}",rotation=45,align="left"))
+    h1$yAxis(list(list(title = list(text = 'Daily download'), min=0, opposite = FALSE), 
+                  list(title = list(text = 'Cummulative download'), min=0, opposite = TRUE)))
+    h1$series(name = 'Daily download', type = 'spline', color = '#000099',
+              data = gsub('"', "", substring(s1.json, 4, nchar(s1.json)-3)))
+    h1$series(name = 'Cummulative download', type = 'spline', color = '#FF0000',
+              data = gsub('"', "", substring(s2.json, 4, nchar(s2.json)-3)), yAxis=1)
+    a <- paste0(capture.output(h1$print()), collapse="")
+    a <- gsub(']" ]', '] ]', a)
+    a <- gsub('\\[ \"\\[', '[ [', a)
+    a  
+  }) 
+  
+    # dd2<-d[,.(dd=.N),by=country]
+    # dd2$ctry<-countrycode::countrycode(dd2$country,"iso2c","country.name")
+    # crosslet(x="country",y="dd",data=dd2)$html()
+    
+})
+
--- cran_logs/get_log.R
+++ cran_logs/get_log.R
@@ -1,7 +1,45 @@
 if (!("data.table" %in% rownames(installed.packages()))){install.packages(data.table)}
 library(data.table)
-old_rlogs<-readRDS("rlogs.RDS")
-rlogs_new<-data.table(date=Sys.Date(),time=format(Sys.time()),v=round(runif(1,0,100)))
+old_rlogs<-readRDS("./cran_logs/rlogs.RDS")
+tbc<-verbose<-TRUE
+# date<-"2019-04-26"
+start_date<-as.Date(max(old_rlogs$date))+1   #"2019-04-26"
+# dates<-seq(start_date,as.Date("2020-01-01"),by="day")
+dates<-seq(start_date,Sys.Date(),by="day")
+
+dlog<-function(date){
+  cat(format(Sys.time()),"---",format(date),"\n")
+  url<-paste0("http://cran-logs.rstudio.com/",gsub("-.*","",date),"/",date,".csv.gz")
+  temp<-tempfile()
+  tryCatch({utils::download.file(url,temp)},
+           error = function(e) {
+             if (verbose){message("Error by the download the log file:",'\n',paste(unlist(e),collapse="\n"))}
+             tbc<-FALSE
+           },
+           warning = function(w) {
+             if (verbose){message("Warning by the download the log file:",'\n',paste(unlist(w),collapse="\n"))}
+           })
+  if (tbc & (file.info(temp)$size>0)){
+    tryCatch({gz<-gzfile(temp,open="rt")},
+             error = function(e) {
+               if (verbose){message("Error by the opening the downloaded gz file:",'\n',paste(unlist(e),collapse="\n"))}
+               tbc<-FALSE
+             },
+             warning = function(w) {
+               if (verbose){message("Warning by the opening the downloaded gz file:",'\n',paste(unlist(w),collapse="\n"))}
+             })
+    if(max(utils::sessionInfo()$otherPkgs$data.table$Version,utils::sessionInfo()$loadedOnly$data.table$Version)>"1.11.7"){
+      logs<-data.table::fread(text=readLines(gz),sep=',',colClasses='character',header=TRUE,stringsAsFactors=F)
+    } else{
+      logs<-data.table::fread(paste(readLines(gz),collapse="\n"),sep=',',colClasses='character',header=TRUE,stringsAsFactors=F)
+    }
+    close(gz)
+    unlink(temp)
+    logs[package=="restatapi"]
+  }  
+}
+# rlogs<-dlog(date)
+rlogs_new<-rbindlist(lapply(dates,dlog))
 rlogs<-rbind(old_rlogs,rlogs_new)
-saveRDS(rlogs,"rlogs.RDS")
-fwrite(rlogs,"rlogs.tsv",sep="\t")
+saveRDS(rlogs,"./cran_logs/rlogs.RDS")
+fwrite(rlogs,"./cran_logs/rlogs.tsv",sep="\t")
