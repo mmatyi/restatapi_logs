8fb9ea3134b47544e3bdf34b0cf3605315b18da4
Mészáros Mátyás <34501282+mmatyi@users.noreply.github.com>
2020-12-24 23:00:51 +0100

    Update crosslet.js

--- docs/js/crosslet.js
+++ docs/js/crosslet.js
@@ -710,10 +710,10 @@ crosslet.changeSelect = function(select, val) {
 crosslet.defaultConfig = {
   map: {
     leaflet: {
-    //  url: "http://{s}.tile.cloudmade.com/{key}/{styleId}/256/{z}/{x}/{y}.png",
+      url: "http://{s}.tile.cloudmade.com/{key}/{styleId}/256/{z}/{x}/{y}.png",
     //  key: "--your key--",
       styleId: 64657,
-   //   attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://www.openstreetmap.org/copyright">ODbL</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
+      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://www.openstreetmap.org/copyright">ODbL</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
     },
     view: {
       center: [51.505, -0.09],
